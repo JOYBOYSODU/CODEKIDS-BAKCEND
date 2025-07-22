526e2cd7401708277f050c4eb035ad24a4e4589c
PRAJESH31 <148569220+JOYBOYSODU@users.noreply.github.com>
2025-07-22 22:16:54 +0530

    update

--- CodeKid-Backend/codekids.services/codekids.services/src/main/java/com/codekids/codekids/services/config/SecurityConfig.java
+++ CodeKid-Backend/codekids.services/codekids.services/src/main/java/com/codekids/codekids/services/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;    
 
 /**
  * Configures the application's security settings, including password hashing
